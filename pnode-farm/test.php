7430bd8a3f8f0e9b55b14c54dc8616b6d2428f69
mrhcon <mrhcon@gmail.com>
2025-07-22 02:25:06 +0000

    Tidy Up - Remove all unneeded files.

--- pnode-farm/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-$details = "Device IP: 194.233.90.20, Port: 80, Timeout: 2s, fsockopen Time: 0.208s, fsockopen Error: Connection refused (111), Ping Time: 0.211s, Status: Online, Method: ping";
-if (preg_match("/(?:Device )?IP: (\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})/", $details, $matches)) {
-    echo "IP: " . $matches[1];
-} else {
-    echo "No IP found";
-}
-?>
