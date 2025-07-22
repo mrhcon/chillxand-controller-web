ff2d751e0efaae7533c79c43534407db8214eeef
mrhcon <michael@mrhconsultantsinc.com>
2025-07-22 13:38:15 -0400

    Enhance status checks.

--- pnode-farm/manual_device_check.php
+++ pnode-farm/manual_device_check.php
@@ -96,7 +96,7 @@ try {
         
         $ch = curl_init();
         curl_setopt_array($ch, [
-            CURLOPT_URL => $health_url,
+            CURLOPT_URL => $_GET[$health_url],
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_TIMEOUT => 5,
             CURLOPT_CONNECTTIMEOUT => 2,
